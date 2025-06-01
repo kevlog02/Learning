44cd8c408e8926c512ee5ea8b73ac98cddf5517a
kevlog02 <kevinandresruro10@gmail.com>
2025-06-01 00:54:06 -0700

    primer avance del curso de js

---
+++ Basic.js/02-datatypes.js
@@ -0,0 +1,36 @@
+//DATOS PRIMITIVOS
+// Cadena de texto (string)
+let myName = "Kevin Andres";
+let alias = "kevlog";
+let email = "kevloog0224@gmail.com"
+
+// Numeros (number)
+let age = "37"; //int
+let height = "1.37";//float
+
+
+// Booleanos (boolean)
+let isTeacher = true;
+let isStudent = false;
+
+// Undefined (variables aun no definidas pero que despues pueden tomar valores)
+let undefinedValue;
+
+// Null (indica que esta variable es nula porque se quiere indicar que un valor sera nulo)
+let nullValue = null;
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(32423444444562654444445624445626544444562654276357465254234);
+let myBigInt2 = 32423444444562654444445624445626544444562654276357465254234n;
+
+//Mostramos los tipos de datos
+console.log(typeof myName); 
+console.log(typeof age); 
+console.log(typeof isTeacher); 
+console.log(typeof undefinedValue); 
+console.log(typeof nullValue); 
+console.log(typeof mySymbol); 
+console.log(typeof myBigInt); 
---
+++ Basic.js/03-beginner-exercises.js
@@ -0,0 +1,49 @@
+// 1. Escribe un comentario en una línea
+
+//increible
+
+// 2. Escribe un comentario en varias líneas
+
+/* 
+Comentario e varias lineas ep
+*/ 
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let myage = 12;
+let myalias = "kevlog";
+let istrue = true;
+let Undefinedvalue = undefined;
+let nullValue = null;
+let mySymbol = Symbol("mysymbol")
+let datoBigint = BigInt(43555555326254642356);
+
+
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(myage); 
+console.log(myalias); 
+console.log(istrue); 
+console.log(nullValue); 
+console.log(mySymbol); 
+console.log(datoBigint); 
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof myage); 
+console.log(typeof myalias); 
+console.log(typeof istrue); 
+console.log(typeof undefinedValue); 
+console.log(typeof nullValue); 
+console.log(typeof mySymbol); 
+console.log(typeof datoBigint); 
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
